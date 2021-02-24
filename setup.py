dc1b1fe9dd72c7413a6a9e83c31ac0d54dd5fc91
Bohao <huangbohao@sensebrain.site>
2021-02-23 16:24:56 -0800

    update version info

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='toolman',
-      version='0.1.13',
+      version='0.1.14',
       description='Python utility tools for research',
       url='https://github.com/bohaohuang/toolman',
       author='bohaohuang',
