4ed0098ac0e70194c34ccbd8c184a8fb2502c551
Bohao <huangbohao@sensebrain.site>
2021-04-19 16:44:53 -0700

    save yaml files

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='toolman',
-      version='0.1.17',
+      version='0.1.17a',
       description='Python utility tools for research',
       url='https://github.com/bohaohuang/toolman',
       author='bohaohuang',
